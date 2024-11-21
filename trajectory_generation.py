3e5e0a4c66dbb3bf5126d24132486c018a0fb8bc
Harrison Bounds <harrison.bounds777@gmail.com>
2024-11-21 17:12:04 -0600

    Gripper functionality added

--- trajectory_generation.py
+++ trajectory_generation.py
@@ -1,7 +1,7 @@
 import modern_robotics as mr
 import numpy as np
 import pandas as pd
-import os
+
 
 Tse_initial = np.array([[1, 0, 0, 0],
                         [0, 1, 0, 0],
@@ -32,26 +32,28 @@ k = 1
 
 output_file = "output_trajectories.csv"
 output_trajectories = []
+gripper_state = False
+gripping = False
 
-def create_trajectories(trajectories, file_path):
-    #Append trajectories if the path already exists
-    if os.path.exists(file_path):
+def create_trajectories(trajectories, gripper_state):
+    if gripper_state:
         for traj in trajectories:
-            output_trajectory = np.array([traj[0][0], traj[0][1], traj[0][2], traj[1][0], traj[1][1], traj[1][2], traj[2][0], traj[2][1], traj[2][2], traj[0][3], traj[1][3], traj[2][3], 0])
-            output_trajectories.append(output_trajectory)
+            output_trajectory = np.array([traj[0][0], traj[0][1], traj[0][2], traj[1][0], traj[1][1], traj[1][2], traj[2][0], traj[2][1], traj[2][2], traj[0][3], traj[1][3], traj[2][3], 1])
+            output_trajectories.append(output_trajectory)    
+            
+            if np.array_equal(traj, trajectories[0]):
+                for _ in range(100): 
+                    output_trajectory = np.array([traj[0][0], traj[0][1], traj[0][2], traj[1][0], traj[1][1], traj[1][2], traj[2][0], traj[2][1], traj[2][2], traj[0][3], traj[1][3], traj[2][3], 1])
+                    output_trajectories.append(output_trajectory)
     else:
         for traj in trajectories:
             output_trajectory = np.array([traj[0][0], traj[0][1], traj[0][2], traj[1][0], traj[1][1], traj[1][2], traj[2][0], traj[2][1], traj[2][2], traj[0][3], traj[1][3], traj[2][3], 0])
             output_trajectories.append(output_trajectory)
-            
-def create_frame_list(Tse_initial, Tsc_initial, Tsc_final, Tce_grasp, Tce_standoff):
-    Tse_standoff_initial = Tsc_initial @ Tce_standoff
-    Tse_grasp_initial = Tsc_initial @ Tce_grasp
-    Tse_standoff_final = Tsc_final @ Tce_standoff
-    Tse_grasp_final = Tsc_final @ Tce_grasp
-    frame_list = []
+        
 
-def Trajectory_Generation(Tse_initial: np.ndarray, Tsc_initial: np.ndarray, Tsc_final: np.ndarray, Tce_grasp: np.ndarray, Tce_standoff: np.ndarray, k: int, output_file: str):
+        
+
+def Trajectory_Generation(Tse_initial: np.ndarray, Tsc_initial: np.ndarray, Tsc_final: np.ndarray, Tce_grasp: np.ndarray, Tce_standoff: np.ndarray, k: int, gripper_state: bool):
     """
     Write output trajectories to a csv for Coppelia Sim to use at each timestep
 
@@ -67,30 +69,32 @@ def Trajectory_Generation(Tse_initial: np.ndarray, Tsc_initial: np.ndarray, Tsc_
     #Get the e-e transform in the s frame
     Tse_standoff_initial = Tsc_initial @ Tce_standoff
     trajectories = mr.ScrewTrajectory(Xstart=Tse_initial, Xend=Tse_standoff_initial, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    create_trajectories(trajectories, gripper_state)
     
     Tse_grasp_initial = Tsc_initial @ Tce_grasp
     
     trajectories = mr.ScrewTrajectory(Xstart=Tse_standoff_initial, Xend=Tse_grasp_initial, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    create_trajectories(trajectories, gripper_state)
     
     trajectories = mr.ScrewTrajectory(Xstart=Tse_grasp_initial, Xend=Tse_standoff_initial, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    gripper_state = True
+    create_trajectories(trajectories, gripper_state)
     
     Tse_standoff_final = Tsc_final @ Tce_standoff
     trajectories = mr.ScrewTrajectory(Xstart=Tse_standoff_initial, Xend=Tse_standoff_final, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    create_trajectories(trajectories, gripper_state)
     
     Tse_grasp_final = Tsc_final @ Tce_grasp
     trajectories = mr.ScrewTrajectory(Xstart=Tse_standoff_final, Xend=Tse_grasp_final, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    create_trajectories(trajectories, gripper_state)
     
     trajectories = mr.ScrewTrajectory(Xstart=Tse_grasp_final, Xend=Tse_standoff_final, Tf=3, N=3*k/0.01, method=3)
-    create_trajectories(trajectories, output_file)
+    gripper_state = False
+    create_trajectories(trajectories, gripper_state)
 
     
     output_trajectories_df = pd.DataFrame(output_trajectories)
     output_trajectories_df.to_csv("output_trajectories.csv", index=False, header=False)
     
-Trajectory_Generation(Tse_initial, Tsc_initial, Tsc_final, Tce_grasp, Tce_standoff, k, output_file)
+Trajectory_Generation(Tse_initial, Tsc_initial, Tsc_final, Tce_grasp, Tce_standoff, k, gripper_state)
     
